7cb2e0a78d6d9b8e94e6f25d49c56afd5030a53f
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-09 02:39:10 +0000

    first commit

---
+++ html/layout/sections/contact.php
@@ -0,0 +1,38 @@
+<section id="contact" class="bg-light py-5">
+            <div class="container">
+                <div class="row">
+                    <h2>Un projet de création web ?</h2>
+                    <h3>Allons en discuter autour d'un café</h3>
+                </div>
+                <div class="row gy-4 mt-5">
+                    <div class="col-12 col-md-5">
+                        <img src="img/grab-a-coffee.jpg" alt="Au café" width="100%">
+                    </div>
+                    <div class="col-12 offset-md-1 col-md-6">
+                        <!--------------------------------contact form start-->
+                        <form action="#" class="row gy-3">
+                            <div class="col-6">
+                                <label for="firstName" class="form-label">Prénom</label>
+                                <input type="text" class="form-control" id="firstName" name="firstName">
+                            </div>
+                            <div class="col-6">
+                                <label for="lastName" class="form-label">Nom</label>
+                                <input type="text" class="form-control" id="lastName" name="lastName">
+                            </div>
+                            <div class="col-12">
+                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                            </div>
+                            <div class="col-12">
+                                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary w-100">Envoyer</button>
+                            </div>
+                        </form>
+                        <!---------------------------------------------contact form end-->
+                    </div>
+                </div>
+            </div>
+        </section>
---
+++ html/layout/sections/presentation.php
@@ -0,0 +1,12 @@
+<section id="ehyua" class="my-5">
+            <div class="container">
+                <div class="row gy-4 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <h1 class="fw-bold">Je suis suis John Doe, Devellopeur et Designer Web</h1>
+                        <h2 class="fw-light">Biennvenu dans mon univers créatif</h2>
+                        <button class="btn btn-primary mt-5">Mon expertise Web</button>
+                    </div>
+                    <div class="col"><img src="img/portrait.jpg" alt="Image de John Doe" class="w-100"></div>
+                </div>
+            </div>
+        </section>
