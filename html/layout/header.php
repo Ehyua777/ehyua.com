a30b9cd56bca33b7d73a50816fa313d2db6f86be
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-12 15:46:13 +0000

    dimanche 12

--- html/layout/header.php
+++ html/layout/header.php
@@ -1,28 +1,28 @@
 <header>
-<nav class="navbar navbar-expand-md bg-light navbar-light fixed-top">
-            <dvi class="container">
-                <a class="navbar-brand text-uppercase fw-bold" href="index.html">
-                    <span class="bg-primary bg-gradient p-1 rounded-3 text-light">John</span>&nbsp;Doe
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#ehyua">Qui suis-je ?</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#expertise">Expertises</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </dvi>
-        </nav>
+    <nav class="navbar navbar-expand-md bg-light navbar-light fixed-top">
+        <dvi class="container">
+            <a class="navbar-brand text-uppercase fw-bold" href="index.php">
+                <span class="bg-primary bg-gradient p-1 rounded-3 text-light">Marcel Ehyua</span>&nbsp;M'BIA
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#ehyua">Qui suis-je ?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#expertise">Expertises</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </dvi>
+    </nav>
 </header>
--- html/layout/sections/presentation.php
+++ html/layout/sections/presentation.php
@@ -1,12 +1,12 @@
 <section id="ehyua" class="my-5">
-            <div class="container">
-                <div class="row gy-4 align-items-center">
-                    <div class="col-12 col-md-6">
-                        <h1 class="fw-bold">Je suis suis John Doe, Devellopeur et Designer Web</h1>
-                        <h2 class="fw-light">Biennvenu dans mon univers créatif</h2>
-                        <button class="btn btn-primary mt-5">Mon expertise Web</button>
-                    </div>
-                    <div class="col"><img src="img/portrait.jpg" alt="Image de John Doe" class="w-100"></div>
-                </div>
+    <div class="container">
+        <div class="row gy-4 align-items-center">
+            <div class="col-12 col-md-6">
+                <h1 class="fw-bold">Je suis suis Marcel Ehyua M'BIA, Devellopeur et Designer Web</h1>
+                <h2 class="fw-light">Biennvenu dans mon univers créatif</h2>
+                <button class="btn btn-primary mt-5">Mon expertise Web</button>
             </div>
-        </section>
+            <div class="col"><img src="https://media.licdn.com/dms/image/C4E03AQFGu1r5e2rizw/profile-displayphoto-shrink_800_800/0/1653871149416?e=1684368000&v=beta&t=lDugaEJE-Z2aq8wMCs-OpmlVe7GMUudVylRADn-zSsg" alt="Image de Marcel Ehyua" class="w-100" height="50%"></div>
+        </div>
+    </div>
+</section>
