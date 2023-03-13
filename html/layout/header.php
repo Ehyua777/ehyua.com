d122b3423b14682c4da7477ae44708da89d140da
N'GUESSAN MARCEL EHUYA M'BIA <lumbrera.ehyua@gmail.com>
2023-03-13 13:09:41 +0000

    modification du nom

--- html/layout/header.php
+++ html/layout/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-md bg-light navbar-light fixed-top">
             <dvi class="container">
                 <a class="navbar-brand text-uppercase fw-bold" href="index.html">
-                    <span class="bg-primary bg-gradient p-1 rounded-3 text-light">John</span>&nbsp;Doe
+                    <span class="bg-primary bg-gradient p-1 rounded-3 text-light">Ehyua</span>&nbsp;M'BIA
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
