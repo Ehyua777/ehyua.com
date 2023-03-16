657d54a8ec2347b9ff3472b77f6e5b3cb5fa8a85
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-16 00:27:49 +0000

    reinitialisation

--- html/layout/main.php
+++
@@ -1,6 +0,0 @@
-<main>
-    <?php include('sections/presentation.php') ?>
-    <?php include('sections/expertise.php') ?>
-    <?php include('sections/portfolio.php') ?>
-    <?php include('sections/contact.php') ?>
-</main>
