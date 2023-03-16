c43c16c8b18fe3a45b0278aba0816b4fce4d5d57
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-16 03:36:28 +0000

    16-03-23:03:35

--- templates/layout/sections/presentation.php
+++ templates/layout/sections/presentation.php
@@ -4,7 +4,9 @@
             <div class="col-12 col-md-6">
                 <h1 class="fw-bold"><?php echo $presentationDatas['h1'] ?></h1>
                 <h2 class="fw-light"><?php echo $presentationDatas['h2'] ?></h2>
-                <button class="btn btn-primary mt-5">Mon expertise Web</button>
+                <button type="submit" class="btn btn-primary mt-5" onclick="window.location.href='#expertises'">
+                    Mon expertise Web
+                </button>
             </div>
             <div class="col"><img src="<?php echo $presentationDatas['photoUrl'] ?>" alt="Image de John Doe" class="w-100"></div>
         </div>
