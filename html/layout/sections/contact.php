c57037955edc30bf8b1705a010d12a7a1fd98216
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-16 00:31:33 +0000

    Reparation

---
+++ html/layout/sections/contact.php
@@ -0,0 +1,18 @@
+<section id="contact" class="bg-light py-5">
+            <div class="container">
+                <div class="row">
+                    <h2>Un projet de developpement ?</h2>
+                    <h3>Allons en discuter autour d'un café</h3>
+                </div>
+                <div class="row gy-4 mt-5">
+                    <div class="col-12 col-md-5">
+                        <img src="img/grab-a-coffee.jpg" alt="Au café" width="100%">
+                    </div>
+                    <div class="col-12 offset-md-1 col-md-6">
+                        <!--------------------------------contact form start-->
+                        <?php include('contact/form.php') ?>
+                        <!---------------------------------------------contact form end-->
+                    </div>
+                </div>
+            </div>
+        </section>
