4e23a6b7f8143cbb6945644145a6070eb87dd45a
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-03-16 03:11:06 +0000

    16-03-23:03:10

--- templates/layout/sections/expertises.php
+++ templates/layout/sections/expertises.php
@@ -7,89 +7,19 @@
         <div class="row mt-5 gy-4">
             <div class="col-12 col-md-7">
                 <!--------------debut progressbar------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-html5 fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">HTML5</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" data-bs-toggle="tooltip" title="75%"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/html.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-css3-alt fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">CSS3</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/css.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-js fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">JS</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-25" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/js.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-php fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">PHP</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/php.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-sass fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">SASS</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/sass.php'); ?>
                 <!--------fin pb-------->
             </div>
             <div class="col-12 offset-md-1 col-md-4">
@@ -102,89 +32,19 @@
             </div>
             <div class="col-12 col-md-7 offset-md-1">
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fab fa-figma fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">Figma</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/figma.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fas fa-photo-video fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">Suite Adobe</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/figma.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fas fa-palette fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">Design UI</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-25" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/design_ui.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fas fa-vial fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">Design UX</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/design_ux.php'); ?>
                 <!--------fin pb-------->
                 <!--------------debut pb------->
-                <div class="p-3 mb-3 bg-white">
-                    <div class="row align-items-center">
-                        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
-                            <div class="d-flex align-items-center">
-                                <i class="fa fa-video fa-2x me-3 text-primary"></i>
-                                <p class="fw-bold m-0">Vidéo</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include('expertises/video.php'); ?>
                 <!--------fin pb-------->
             </div>
         </div>
---
+++ templates/layout/sections/expertises/css.php
@@ -0,0 +1,15 @@
+<div class="p-3 mb-3 bg-white">
+    <div class="row align-items-center">
+        <div class="col-5 col-sm-4 col-md-3 co-xl-2">
+            <div class="d-flex align-items-center">
+                <i class="fab fa-css3-alt fa-2x me-3 text-primary"></i>
+                <p class="fw-bold m-0">CSS3</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="progress">
+                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    </div>
+</div>
