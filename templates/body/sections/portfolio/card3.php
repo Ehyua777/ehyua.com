eeac20cf15b7639dba37296e88eb07e3c724bb17
Ehyua777 <lumbrera.ehyua@gmail.com>
2023-04-07 11:26:12 +0000

    07-04-2023-2

---
+++ templates/body/sections/portfolio/card3.php
@@ -0,0 +1,60 @@
+<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="card">
+        <img src="https://picsum.photos/300/150?random=3" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a class="btn btn-primary" data-bs-toggle="offcanvas" data-bs-target="#infoProjet3">Go somewhere</a>
+            <!-------------------CANVAS START---------------->
+            <div class="offcanvas offcanvas-start h-100" tabindex="-1" id="infoProjet3" aria-labelledby="titleProject1">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="titleProject1">Projet 3</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <!------carousel 1 start-->
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://picsum.photos/1920/1080?random=7" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/1920/1080?random=8" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Second slide label</h5>
+                                    <p>Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/1920/1080?random=9" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <!----------------carousel 3 end-->
+                </div>
+            </div>
+            <!----------------CANVAS END-------------------------->
+        </div>
+    </div>
+</div>
---
+++ templates/body/sections/portfolio/intro.php
@@ -0,0 +1,4 @@
+<div class="row">
+    <h2>Mon portfolio</h2>
+    <h3>Projets perso et pro</h3>
+</div>
